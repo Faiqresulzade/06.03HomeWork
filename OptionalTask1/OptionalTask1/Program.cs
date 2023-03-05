ad1a25b4d3bed33c1f63e4b653b17f12e891959e
Faiqresulzade <faigbr@code.edu.az>
2023-03-06 02:18:16 +0400

    added 06.03.2023

---
+++ OptionalTask1/OptionalTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = { 2, 4, 1, 9, 7 };
+int change_number = 0;
+for (int i = 0; i < numbers.Length-1; i++)
+{
+    if (numbers[i] > numbers[i + 1])
+    {
+        change_number = numbers[i];
+        numbers[i] = numbers[i + 1];
+        numbers[i + 1] = change_number;
+      //  i = -1;
+    }
+}
+//Console.WriteLine(numbers[i]);
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine(numbers[i]);
+}
---
+++ OptionalTask2/OptionalTask2/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] numbers = { 2, 3, 6, 9, 12,19,21,22,23};
+int searched_number = 23;
+double step_double = Math.Sqrt(numbers.Length);
+int step = (int)step_double;
+int i = 0;
+while (searched_number != numbers[i])
+{
+    if (searched_number > numbers[i])
+    {
+        i = i + step;
+        numbers[i]=searched_number;
+        if (i > numbers.Length - 1 && searched_number == numbers[numbers.Length - 1])
+        {
+            Console.WriteLine(numbers[numbers.Length - 1]);
+            return;
+        }
+        if (i > numbers.Length - 1)
+        {
+            Console.WriteLine("Bele reqem yoxdur!!");
+            return;
+        }
+    }
+    if(searched_number < numbers[i])
+    {
+        i = step--;
+    }
+}
+if(searched_number == numbers[i])
+{
+    Console.WriteLine(numbers[i]);
+}
